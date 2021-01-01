d3e26687283d748c1aeefe43755297f6dbca4c99
eduardolibindo <57646249+eduardolibindo@users.noreply.github.com>
2020-12-31 22:28:08 -0300

    new

--- src/app/maps/map-places/map-places.component.ts
+++ src/app/maps/map-places/map-places.component.ts
@@ -1,27 +1,7 @@
-import { Component, Input, ViewChild, NgZone, OnInit } from '@angular/core';
+import { Component, Input, ViewChild, NgZone, OnInit, OnChanges } from '@angular/core';
 import { MapsAPILoader, AgmMap, GoogleMapsAPIWrapper, MouseEvent } from '@agm/core';
 
 declare var google: any;
-
-interface Marker {
-  lat: number;
-  lng: number;
-  label?: string;
-  draggable: boolean;
-}
-
-interface Location {
-  lat: number;
-  lng: number;
-  viewport?: Object;
-  zoom: number;
-  address_level_1?: string;
-  address_level_2?: string;
-  address_country?: string;
-  address_zip?: string;
-  address_state?: string;
-  marker?: Marker;
-}
 
 @Component({
   selector: 'app-map-places',
@@ -30,182 +10,48 @@ interface Location {
 })
 
 export class MapPlacesComponent implements OnInit {
-  texto = 'Mapa da localização dos alunos';
-  circleRadius: number = 2000;
-  geocoder: any;
 
-  public location: Location = {
-    lat: -29.135762228832924,
-    lng: -56.54889502311831,
-    marker: {
-      lat: -29.135762228832924,
-      lng: -56.54889502311831,
-      draggable: true
-    },
-    zoom: 14
-  };
-
-
-
-  @ViewChild(AgmMap) map: AgmMap;
-
-
-  constructor(
-    public mapsApiLoader: MapsAPILoader,
-    private zone: NgZone,
-    private wrapper: GoogleMapsAPIWrapper) {
-    this.mapsApiLoader = mapsApiLoader;
-    this.zone = zone;
-    this.wrapper = wrapper;
-    this.mapsApiLoader.load().then(() => {
-      this.geocoder = new google.maps.Geocoder();
-    });
-  }
+  constructor() { }
 
   ngOnInit() {
-    this.location.marker.draggable = true;
-  }
 
-  updateOnMap() {
-    let full_address: string = this.location.address_level_1 || ""
-    if (this.location.address_level_2) full_address = full_address + " " + this.location.address_level_2
-    if (this.location.address_state) full_address = full_address + " " + this.location.address_state
-    if (this.location.address_country) full_address = full_address + " " + this.location.address_country
-
-    this.findLocation(full_address);
-  }
-
-  findLocation(address) {
-    if (!this.geocoder) this.geocoder = new google.maps.Geocoder()
-    this.geocoder.geocode({
-      'address': address
-    }, (results, status) => {
-      console.log(results);
-      if (status == google.maps.GeocoderStatus.OK) {
-        for (var i = 0; i < results[0].address_components.length; i++) {
-          let types = results[0].address_components[i].types
-
-          if (types.indexOf('locality') != -1) {
-            this.location.address_level_2 = results[0].address_components[i].long_name
-          }
-          if (types.indexOf('country') != -1) {
-            this.location.address_country = results[0].address_components[i].long_name
-          }
-          if (types.indexOf('postal_code') != -1) {
-            this.location.address_zip = results[0].address_components[i].long_name
-          }
-          if (types.indexOf('administrative_area_level_1') != -1) {
-            this.location.address_state = results[0].address_components[i].long_name
-          }
-        }
-
-        if (results[0].geometry.location) {
-          this.location.lat = results[0].geometry.location.lat();
-          this.location.lng = results[0].geometry.location.lng();
-          this.location.marker.lat = results[0].geometry.location.lat();
-          this.location.marker.lng = results[0].geometry.location.lng();
-          this.location.marker.draggable = true;
-          this.location.viewport = results[0].geometry.viewport;
-        }
-
-        this.map.triggerResize()
-      } else {
-        alert("Sorry, this search produced no results.");
-      }
-    })
-  }
-
-  clickedMarker(label: string, index: number) {
-    console.log(`clicked the marker: ${label || index}`)
-  }
-
-  mapClicked($event: MouseEvent) {
-    this.markers.push({
-      lat: $event.coords.lat,
-      lng: $event.coords.lng,
-      draggable: false
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 7,
+      center: { lat: 41.85, lng: -87.65 }
     });
-  }
+    directionsDisplay.setMap(map);
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
 
-  markerDragEnd(m: Marker, $event: MouseEvent) {
-    this.location.marker.lat = $event.coords.lat;
-    this.location.marker.lng = $event.coords.lng;
-    this.findAddressByCoordinates();
-    console.log('dragEnd', m, $event);
-  }
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
 
-  markers: Marker[] = [
-	  {
-		  lat: -29.127939913588087,
-		  lng: -56.55371355529949,
-		  label: 'Eduardo Libindo',
-		  draggable: false
-	  },
-	  {
-		  lat: -29.1333755375587,
-		  lng: -56.558391327801935,
-		  label: 'Estudante Sigtei 1',
-		  draggable: false
-	  },
-	  {
-		  lat: -29.137723829809183,
-		  lng: -56.55182528016088,
-		  label: 'Estudante Sigtei 2',
-		  draggable: false
-	  }
-  ]
+      const waypts = [];
+      const checkboxArray: any[] = [
+        'winnipeg', 'regina', 'calgary'
+      ];
+      for (var i = 0; i < checkboxArray.length; i++) {
 
-  findAddressByCoordinates() {
-    this.geocoder.geocode({
-      'location': {
-        lat: this.location.marker.lat,
-        lng: this.location.marker.lng
-      }
-    }, (results, status) => {
-      this.decomposeAddressComponents(results);
-    })
-  }
+        waypts.push({
+          location: checkboxArray[i],
+          stopover: true
+        });
 
-  decomposeAddressComponents(addressArray) {
-    if (addressArray.length == 0) return false;
-    let address = addressArray[0].address_components;
+      }
 
-    for (let element of address) {
-      if (element.length == 0 && !element['types']) continue
-
-      if (element['types'].indexOf('street_number') > -1) {
-        this.location.address_level_1 = element['long_name'];
-        continue;
-      }
-      if (element['types'].indexOf('route') > -1) {
-        this.location.address_level_1 += ', ' + element['long_name'];
-        continue;
-      }
-      if (element['types'].indexOf('locality') > -1) {
-        this.location.address_level_2 = element['long_name'];
-        continue;
-      }
-      if (element['types'].indexOf('administrative_area_level_1') > -1) {
-        this.location.address_state = element['long_name'];
-        continue;
-      }
-      if (element['types'].indexOf('country') > -1) {
-        this.location.address_country = element['long_name'];
-        continue;
-      }
-      if (element['types'].indexOf('postal_code') > -1) {
-        this.location.address_zip = element['long_name'];
-        continue;
-      }
+      directionsService.route({
+        origin: { lat: 41.85, lng: -87.65 },
+        destination: { lat: 49.3, lng: -123.12 },
+        waypoints: waypts,
+        optimizeWaypoints: true,
+        travelMode: 'DRIVING'
+      }, function (response, status) {
+        if (status === 'OK') {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
     }
   }
-
-  milesToRadius(value) {
-    this.circleRadius = value / 0.00062137;
-  }
-
-  circleRadiusInMiles() {
-    return this.circleRadius * 0.00062137;
-  }
-
 }
